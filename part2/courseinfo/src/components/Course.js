9b1de318e3bdcd1f0e9f4a864eea8791599fbff6
Devin Lizardi <dlizardi@berkeley.edu>
2022-08-08 11:53:50 -0400

    2.2 complete

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,22 +1,29 @@
-import React from "react"
+import React from "react";
 
-const Header = ({ name }) => <h1>{name}</h1>
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
+const Header = ({ name }) => <h1>{name}</h1>;
+const Total = ({ parts }) => {
+  let sum = 0;
+  parts.forEach((part) => (sum += part.exercises));
+  return <b>total of {sum} exercises</b>;
+};
 
 const Content = ({ parts }) => {
-  return parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)
-}
+  return parts.map((part) => (
+    <p key={part.id}>
+      {part.name} {part.exercises}
+    </p>
+  ));
+};
 
 const Course = ({ course }) => {
   console.log(course);
-  return <> 
-    <Header name={course.name}/>
-    <Content parts={course.parts} />
-  </>
-}
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
 
-export default Course
+export default Course;
