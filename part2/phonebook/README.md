cf34a352a783741110d5386a1c2c68e5442dccad
Devin Lizardi <dlizardi@berkeley.edu>
2022-08-10 11:44:25 -0400

    2.11 complete

--- part2/phonebook/README.md
+++ part2/phonebook/README.md
@@ -7,6 +7,14 @@
 - [x] 2.9*
 - [x] 2.10
 
+- [x] 2.11
+- [ ] 2.12*
+- [ ] 2.13*
+- [ ] 2.14*
+
 ### Development Notes
 #### 2.9*
 Setting up the state objects wasn't too hard a challenge, but passing the filter state to the necessary component and using it to filter the array was tricky. There might be a better way to use the filter input, I was a little concerned that String#filter only creates a shallow copy, but it seems to work just fine (for now).
+
+#### 2.11
+Really enjoyed setting up a JSON-server even though it's super simple. I ran into a bunch of npm warnings that required auditing with npm but on googling seemed to be pretty normal and wouldn't impact the project at this phase (or any, really). Was satisfying to see things work!
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import PersonForm from "./components/PersonForm"
 import NumberDisplay from "./components/NumberDisplay"
+import axios from 'axios'
 
 const Filter = ({ newFilter, handleFilterChange }) => {
   return (
@@ -11,15 +12,18 @@ const Filter = ({ newFilter, handleFilterChange }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [newFilter, setNewFilter] = useState("")
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(res => {
+        setPersons(res.data)
+      })
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault()
