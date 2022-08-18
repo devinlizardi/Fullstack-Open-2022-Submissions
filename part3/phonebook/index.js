82f85de4b55c191fe03ed3637e68ecc86627a226
Devin Lizardi <dlizardi@berkeley.edu>
2022-08-17 21:59:10 -0400

    3.3 complete

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -42,6 +42,18 @@ app.get("/api/persons", (req, res) => {
   res.json(persons)
 })
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (!person) {
+    res.status(404).end()
+    return
+  }
+
+  res.json(person)
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
