464e6d91b83419e2d72311dd419cbd80e67cc8f8
Devin Lizardi <dlizardi@berkeley.edu>
2022-08-24 12:04:08 -0400

    help

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -117,5 +117,5 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-  console.log("is this running at all?")
+  console.log(app)
 })
