56ba601ef418a8fccbe81ccdc6cacf28db9fba62
Devin Lizardi <dlizardi@berkeley.edu>
2022-09-16 19:10:18 -0400

    More cleaning

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,23 +1,23 @@
-const logger = require('./logger')
+const logger = require("./logger")
 
 const reqLogger = (req, res, next) => {
-  logger.info('Method:', req.method)
-  logger.info('Path:  ', req.path)
-  logger.info('Body:  ', req.body)
-  logger.info('---')
+  logger.info("Method:", req.method)
+  logger.info("Path:  ", req.path)
+  logger.info("Body:  ", req.body)
+  logger.info("---")
   next()
 }
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
+  res.status(404).send({ error: "unknown endpoint" })
 }
 
 const errorHandler = (error, req, res, next) => {
   logger.error(error.message)
 
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
     return res.status(400).json({ error: error.message })
   }
 
@@ -27,5 +27,5 @@ const errorHandler = (error, req, res, next) => {
 module.exports = {
   reqLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
 }
