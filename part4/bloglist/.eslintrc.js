3681bfcd6dff0b23fba6a17f239da35d655571a9
Devin Lizardi <dlizardi@berkeley.edu>
2022-09-19 13:33:45 -0400

    Added eslint to part 4

---
+++ part4/bloglist/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es2021": true,
+        "jest": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
