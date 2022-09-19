5625ea9ee46502e91eb000f7dbce38b471130ddc
Devin Lizardi <dlizardi@berkeley.edu>
2022-09-19 14:04:22 -0400

    4.5 complete

--- part4/bloglist/tests/blogs.test.js
+++ part4/bloglist/tests/blogs.test.js
@@ -1,44 +1,53 @@
-const listHelper = require('../utils/list_helper')
+const listHelper = require("../utils/list_helper")
+const exampleBlogs = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+]
 
-test('dummy returns one', () => {
-  const blogs = []
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
-})
-
-describe('total likes', () => {
-  const exampleBlogs = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  test('of empty blogs is zero', ()=> {
+describe("total likes", () => {
+  test("of empty blogs is zero", () => {
     expect(listHelper.totalLikes([])).toBe(0)
   })
 
-  test('of n >= 1 blogs to be added', () => {
+  test("of n >= 1 blogs to be added", () => {
     expect(listHelper.totalLikes(exampleBlogs)).toBe(17)
+  })
+})
+
+describe("favorite blog", () => {
+  const favExample = {
+    title: "React patterns",
+    author: "Michael Chan",
+    likes: 7,
+  }
+
+  test("of empty blogs is null", () => {
+    expect(listHelper.favoriteBlog([])).toEqual(null)
+  })
+
+  test("of example blogs to be React Patterns", () => {
+    expect(listHelper.favoriteBlog(exampleBlogs)).toEqual(favExample)
   })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,15 +1,28 @@
-const dummy = () => {
-  return 1
-}
-
 const totalLikes = (blogs) => {
   let total = 0
 
-  blogs.forEach(el => {
+  blogs.forEach((el) => {
     total += el.likes
   })
 
   return total
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  let { title, author, likes } = blogs[0]
+
+  blogs.forEach((el) => {
+    if (el.likes >= likes) {
+      title = el.title
+      author = el.author
+      likes = el.likes
+    }
+  })
+
+  return { title: title, author: author, likes: likes }
+}
+
+module.exports = { totalLikes, favoriteBlog }
