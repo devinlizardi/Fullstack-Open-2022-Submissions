8b74e3b5ec76b755d853294f9482928e6fc62c20
Devin Lizardi <dlizardi@berkeley.edu>
2022-10-12 11:48:40 -0400

    Establish necessary params

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,1 +1,12 @@
-console.log("running")
+/*
+BMI is calculated as body mass divided by square of body height
+@param: height in cm
+@param: weight in kg
+@return bmi in kg/m^2
+*/
+
+const calculateBmi = (height: Number, weight: Number): string => {
+  return "hello"
+}
+
+console.log(calculateBmi)
