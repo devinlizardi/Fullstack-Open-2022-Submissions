fff13c3f7116158cd95eba3da3cbd362da58de90
Devin Lizardi <dlizardi@berkeley.edu>
2022-10-12 12:33:25 -0400

    complete: 9.1

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -6,7 +6,16 @@ BMI is calculated as body mass divided by square of body height
 */
 
 const calculateBmi = (height: number, weight: number): string => {
-  return "hello"
+  const bmi = weight / ((height / 100) ** 2)
+
+  if (bmi < 16) { return 'Underweight (severe thinness)' }
+  if (bmi <= 16.9) { return 'Underweight (moderate thinness)' }
+  if (bmi <= 18.4) { return 'Underweight (mild thinness)' }
+  if (bmi <= 24.9) { return 'Normal range' }
+  if (bmi <= 29.9) { return 'Overweight (pre-obese)' }
+  if (bmi <= 34.9) { return 'Obese (class I)' }
+  if (bmi <= 39.9) { return 'Obese (class II)' }
+  else { return 'Obese (class III)' }
 }
 
-console.log(calculateBmi)
+console.log(calculateBmi(180, 74))
