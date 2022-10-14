0f0f4d881954de70e44cf8ac7d43a1abe9351041
Devin Lizardi <dlizardi@berkeley.edu>
2022-10-14 13:13:49 -0700

    fix: formatting

--- part9/index.ts
+++ part9/index.ts
@@ -1,23 +1,25 @@
-import express from 'express'
-import bmiCalculator from './bmiCalculator'
-import { HealthData } from './bmiCalculator'
+import express from "express"
+import bmiCalculator from "./bmiCalculator"
+import { HealthData } from "./bmiCalculator"
 const PORT = 3001
 const app = express()
 
-app.get('/hello', (_req: any, res: any) => {
-  res.send('Hello Full Stack!').end()
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!").end()
 })
 
-app.get('/bmi', (req: any, res: any) => {
-  const bmi: HealthData | Error = bmiCalculator(req.query)
-  if (bmi instanceof Error) {
-    res.statusMessage = 'Missing Params'
-    res.status(400).send({
-      error: 'could not find weight and height in request query'
-    }).end()
-    return
+app.get("/bmi", (req, res) => {
+  const calculatorResponse: HealthData | Error = bmiCalculator(req.query)
+  if (calculatorResponse instanceof Error) {
+    res.statusMessage = "Missing Params"
+    res
+      .status(400)
+      .send({
+        error: "could not find weight and height in request query",
+      })
+      .end()
   } else {
-    res.json(bmi)
+    res.json(calculatorResponse)
   }
 })
 
