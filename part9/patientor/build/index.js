93c2b7174e4b7b595cae09f7d11ce90b5e404e13
Devin Lizardi <dlizardi@berkeley.edu>
2022-10-15 16:56:59 -0700

    fix: added cors

--- part9/patientor/build/index.js
+++ part9/patientor/build/index.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const PORT = 3001;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.get("/ping", (_req, res) => {
+app.use((0, cors_1.default)());
+app.get("/api/ping", (_req, res) => {
     console.log("pinged");
     res.send("pong");
 });
